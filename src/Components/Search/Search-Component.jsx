4fe7a79420881be98d22f99a97174823a7135721
Ali Rasheed <alirasheed@Alis-MacBook-Pro-2.local>
2020-09-29 09:21:07 -0500

    filter list extracted

--- src/Components/Search/Search-Component.jsx
+++ src/Components/Search/Search-Component.jsx
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 import './Search-Component.css';
 import api from '../../Api/Api';
+import Utils from '../../Utils/Utils';
 
 class Search extends Component {
     constructor(props){
         super(props);
         this.state = {
-          completeResturauntList: []
+          completeResturauntList: [],
+          filteredList: [],
+          genreList: [],
+          stateList: [],
+          isStateFilterActive: false,
+          isGenreFilterActive: false,
+          inputValue: '',
+          optionsSelectedValue: '',
+          filterByState: '',
+          filterByGenre: '',
+          genreSelected: '',
+          stateSelected: '',
+          defaultValue: 'All',
+          searchedList: []
         }
       }
 
@@ -15,8 +29,12 @@ class Search extends Component {
           const retrievedList = res;
           console.log(retrievedList);
           const alphabeticallySortedList = this.alphabeticalSort(retrievedList);
+          const genreList = this.getGenreList(retrievedList);
+          const stateList = this.getStateList(retrievedList);
           this.setState({
-            completeResturauntList: alphabeticallySortedList
+            completeResturauntList: alphabeticallySortedList,
+            genreList,
+            stateList
           })
         })
       }
@@ -24,13 +42,49 @@ class Search extends Component {
       alphabeticalSort = (list) => {
         return list.sort((a, b) => a.name.localeCompare(b.name));
       }
+
+      getGenreList = (list) => {
+        const refinedList = Utils.extractGenreList(list);
+        return refinedList;
+      }
+      
+      getStateList = (list) => {
+        const stateList = Utils.extractStateList(list);
+        return stateList;
+      }
+
   render() {
+    const { filteredList, genreList, stateList } = this.state;
     const listItems = this.state.completeResturauntList.map((item) =>
         <li key={item.id}>{item.name} | {item.genre} | {item.state}</li>
     );
     return (
         <div>
+          <div className="top-row">
           <input placeholder="Enter your search" type="text" />
+            <div>
+            <select defaultValue={this.state.defaultValue} onChange={this.handleGenreFilterChange}>
+              <option value="All" disabled>Genre</option>
+              <option value="Default">All</option>
+                {
+                  genreList.map((genre, key) => {
+                  return <option key={key} value={genre.value}>{genre}</option>;
+                  })
+                }
+              </select>
+            </div>
+            <div>
+              <select defaultValue={this.state.defaultValue} onChange={this.handleStateFilterChange}>
+                <option value="All" disabled>State</option>
+                <option value="Default">All</option>
+                {
+                  stateList.map((genre, key) => {
+                  return <option key={key} value={genre.value}>{genre}</option>;
+                  })
+                }
+              </select>
+            </div>
+          </div>
           <div>
           {listItems.length > 0 ? (
             <ul className="list">
