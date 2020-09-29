d207579ce11445f9b4c8289ee2751522f67516e7
Ali Rasheed <alirasheed@Alis-MacBook-Pro-2.local>
2020-09-29 07:31:05 -0500

    file structure added

---
+++ src/Components/Table/Table-Component.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function Table() {
+  return (
+    <div>
+      <p>Table Component</p>
+    </div>
+  );
+}
+
+export default Table;
